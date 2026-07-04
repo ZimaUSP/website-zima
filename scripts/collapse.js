857d1404c945e37f8af65d49d831959786751e1e
Milena Koronfli Marchesani Pereira <milenakoronfli@gmail.com>
2026-07-04 20:23:58 -0300

    Ajustes de estilo e funcionalidade em equipe

--- scripts/collapse.js
+++ scripts/collapse.js
@@ -1,9 +1,11 @@
 const bar = document.querySelectorAll("div.collapsible-bar");
 
 function collapse(event) {
-    event.target.parentElement.classList.toggle("collapse");
+    event.currentTarget.classList.toggle("collapse");
 }
 
 bar.forEach(e => {
 	e.addEventListener("click", collapse);
 })
+
+
